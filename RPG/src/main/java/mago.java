d455ed744817c962a86a4a0bba19d846b5306447
“Alan” <alanbmc00@gmail.com>
2023-09-21 15:44:46 -0400

    atualizando classe mago

--- RPG/src/main/java/mago.java
+++ RPG/src/main/java/mago.java
@@ -1,12 +1,15 @@
 
 public abstract class  mago extends personagem implements interface_status_mago {
     // CRIAR 3 MAGIAS ESPECIAL(PROTETED)    
-   
-
-    public mago( int vida, int danoF, String nome, int resistenciaF, int resistenciaM, int danoM, int raca, int sexo, String descricao, String magia) {
+   protected int overlord;
+   protected int sorcere;
+   protected int spelholler;
+    public mago( int overlord, int sorcere, int spelholler,int vida, int danoF, String nome, int resistenciaF, int resistenciaM, int danoM, int raca, int sexo, String descricao, String magia) {
         super(vida, danoF, nome, resistenciaF, resistenciaM, danoM, raca, sexo, 
                 descricao,  magia);
-      
+        this.overlord = overlord;
+        this.sorcere = sorcere;
+        this.spelholler = spelholler;
     }
 
    
@@ -20,10 +23,48 @@ public abstract class  mago extends personagem implements interface_status_mago 
         this.vida = 3000;
     }
 
-    public void overlord( ){
-        super.setRaca(1);
 
-        
+    
+    
+    public void sorcere(){
+        super.setRaca(1);
+        super.setDescricao ( "O Sorcerer é muito perplexo, sendo conhecedor do poder dos elementais, "
+                + "e usando magias extremas com eles. Ele auxilia seus aliados colocando em chamas seus"
+                + " inimigos de uma longa distância usando Prominence. Inimigos do Sorceress são colocados "
+                + "para dormir com Sleeping Cloud. (* Ótimo veneno. Seu dano principal é baseado no fogo. Seu dano "
+                + "é bom e sua velocidade de locomoção é ruim. *)"
+                + "Por ser mago possui baixa resistencia fisica, mas em locais adequados pode se sobressair sobre seus "
+                + "oponentes");
+        super.setMagia (" Prominence"
+                + "Baseado em fogo possui poder 40"
+                + "Prominence é uma bola de fogo que ao se desperida se torna "
+                + "mortal foi se afinal ao longo do caminho causando muito dano ao inimigo");
+       // habilidade especial(magia)
+       this.sorcere = 1;
+  
+    }
+    
+    
+    
+    public void spelholler(){
+         super.setRaca(2);
+         super.setDescricao("Spellhowler (SH) são feiticeiros ofensivos (nukers)"
+                 + " dos Elfos Negros. Eles usam vestes e podem usar qualquer arma,"
+                 + " mesmo que geralmente prefiram aqueles com alto M.Atk. Elfos negros foram muito gananciosos e entraram numa guerra com os elfos brancos"
+                 + "Por isso se descolaram para a floresta negra, e se aperfeiçoaram na arte da magia negra."
+                 + "Por ser mago possui baixa resistencia fisica, porem em local adequado se torna muito forte."
+                 + "O Spellhowler usa o mais alto grau de magia elemental. Uma mistura de Feiticeiro e Necromante,"
+                 + " ele lança magia tomando emprestado o poder do vento. O Spellhowler causa fortes danos com a Tempestade,"
+                 + " que cria um tornado ao redor de seu alvo, ou ele pode usar o Furacão para disparar lâminas de vento à distância.");
+               // habilidade especial(magia)
+        super.setMagia("Cria um turbilhão de destruição. HURRICANE(FURACÃO)"
+                + "Alto poder magico, porem demora muito para ser lançado.");
+        this.spelholler = 2;
+
+    }
+    
+        public void overlord( ){
+        super.setRaca(3);
         super.setDescricao(    "É literalmente a classe para combate PvP. "
                 + "O Dominator além de possuir buffs massivos insanos para seus companheiros "
                 + "de clã ele pode enfraquecer os membros do clã inimigo limitando seu CP ou diminuindo "
@@ -37,39 +78,7 @@ public abstract class  mago extends personagem implements interface_status_mago 
                 + "Baseado em vampirismo magico"
                 + "Suga o HP do inimigo convertando para si proprio"
                 + "";
-        // habilidade especial(magia)
-
-        //  PARA BATALHAS COMPARA MAGIAS
-        
+        this.overlord = 3;
     }
-    
-    
-    public void sorcere(){
-        super.setRaca(2);
-        super.setDescricao ( "O Sorcerer é muito perplexo, sendo conhecedor do poder dos elementais, "
-                + "e usando magias extremas com eles. Ele auxilia seus aliados colocando em chamas seus"
-                + " inimigos de uma longa distância usando Prominence. Inimigos do Sorceress são colocados "
-                + "para dormir com Sleeping Cloud. (* Ótimo veneno. Seu dano principal é baseado no fogo. Seu dano "
-                + "é bom e sua velocidade de locomoção é ruim. *)");
-        super.setMagia (" Prominence"
-                + "Baseado em fogo possui poder 40"
-                + "Prominence é uma bola de fogo que ao se desperida se torna "
-                + "mortal foi se afinal ao longo do caminho causando muito dano ao inimigo");
-       // habilidade especial(magia)
-       
-       
-        
-        
-        
-    }
-    
-    public void spelholler(){
-         super.setRaca(1);
-
-               // habilidade especial(magia)
-
-    }
-    
-    
     
 }
